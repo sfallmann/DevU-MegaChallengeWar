5ecf4443a5cf845a82234332a08c7523fefdc78c
sfallmann <jorundr@hotmail.com>
2018-07-19 09:18:19 -0400

    Added Deck class

---
+++ MegaChallengeWar/Classes/Deck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar.Classes
+{
+    public class Deck
+    {
+        private List<Card> _cards;
+
+        public List<Card> Cards
+        {
+            get { return _cards; }
+        }
+
+        public void AddCard(Card card)
+        {
+            this._cards.Add(card);
+        }
+
+        public void RemoveCard(Card card)
+        {
+            this._cards.Remove(card);
+        }
+
+        public List<Card> GetCards()
+        {
+            return this._cards;
+        }
+
+    }
+}
