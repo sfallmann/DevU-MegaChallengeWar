faaa2cace6bb825f3b2fc30cf66b3a9ba57f3926
sfallmann <jorundr@hotmail.com>
2018-07-19 09:44:29 -0400

    Added Player class

---
+++ MegaChallengeWar/Classes/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaChallengeWar.Classes
+{
+    public class Player
+    {
+        private string _name;
+        private Deck _deck;
+
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public Deck Deck
+        {
+            get { return _deck; }
+        }
+
+        public Player(string name, Deck deck)
+        {
+            this._name = name;
+            this._deck = deck;
+        }
+
+    }
+}
